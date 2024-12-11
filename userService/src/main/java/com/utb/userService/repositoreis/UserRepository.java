e46263ba7d4ae43873de85b6595a5ac936bcd758
Srikant <srikantkumar2021@gmail.com>
2024-12-11 14:40:04 +0530

    removing old userservice

--- userService/src/main/java/com/utb/userService/repositoreis/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utb.userService.repositoreis;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.utb.userService.entities.User;
-
-public interface UserRepository extends JpaRepository<User, String>{
-
-}
